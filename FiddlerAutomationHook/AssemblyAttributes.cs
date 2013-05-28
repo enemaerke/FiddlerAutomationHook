031a021a34f3e218a336e787a12a17f029d7748a
Søren Enemærke <soren.enemaerke@gmail.com>
2013-05-28 19:00:47 +0200

    Simple wrapper project with a few dependencies

---
+++ FiddlerAutomationHook/AssemblyAttributes.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fiddler;
+
+[assembly:Fiddler.RequiredVersion("2.3.4.5")]
+
---
+++ FiddlerAutomationHook/SimpleExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fiddler;
+
+namespace FiddlerAutomationHook
+{
+  public class SimpleExtension : IAutoTamper
+  {
+    public void OnLoad()
+    {
+    }
+
+    public void OnBeforeUnload()
+    {
+    }
+
+    public void AutoTamperRequestBefore(Session oSession)
+    {
+      oSession.oRequest["User-Agent"] = "Violin";
+    }
+
+    public void AutoTamperRequestAfter(Session oSession)
+    {
+    }
+
+    public void AutoTamperResponseBefore(Session oSession)
+    {
+    }
+
+    public void AutoTamperResponseAfter(Session oSession)
+    {
+    }
+
+    public void OnBeforeReturningError(Session oSession)
+    {
+    }
+  }
+}
